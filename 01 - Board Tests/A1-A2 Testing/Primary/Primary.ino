db98759bbdb9e08eca7ef6b8029d0f5d9fdedce5
damianCianci <140140529+damianCianci@users.noreply.github.com>
2023-07-20 23:41:21 -0700

    Basic PCB testing

---
+++ 01 - Board Tests/A1-A2 Testing/Primary/Primary.ino
@@ -0,0 +1,30 @@
+
+
+const int bright01 = 130;
+const int ard1_red = 9;
+const int ard1_blue = 8;
+const int glow01_on = 10;
+const int glow01_off = 300;
+
+void setup() {
+  // put your setup code here, to run once:
+
+
+pinMode(ard1_red, OUTPUT);
+pinMode(ard1_blue, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+analogWrite(ard1_red,bright01);
+delay(glow01_on);
+analogWrite(ard1_red,0);
+delay(glow01_off);
+
+digitalWrite(ard1_blue,HIGH);
+delay(glow01_on);
+digitalWrite(ard1_blue,LOW);
+delay(glow01_off);
+
+}
